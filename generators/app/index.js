026ceebcf72886b652b6a4ff9720975940d1ae1d
Steven Follis <steven_follis@live.com>
2017-06-23 07:58:51 -0400

    #1 Added v1 endpoint support + refactor

--- generators/app/index.js
+++ generators/app/index.js
@@ -17,10 +17,17 @@ module.exports = class extends Generator {
     },
     {
       type: 'list',
-      choices: ['Version 1 Endpoint (created at http://portal.azure.com)', 'Version 2 Endpoint (created at http://apps.dev.microsoft.com)'],
+      choices: [{
+        name: 'Version 1 Endpoint (created at http://portal.azure.com)',
+        value: 'v1'
+      },
+      {
+        name: 'Version 2 Endpoint (created at http://apps.dev.microsoft.com)',
+        value: 'v2'
+      }],
       name: 'aadVersion',
       message: `Will your application use Azure Active Directory's version 1 or version 2 endpoints?`,
-      default: 'Version 2 Endpoint (created at http://apps.dev.microsoft.com)'
+      default: 'v2'
     },
     {
       type: 'input',
@@ -52,15 +59,24 @@ module.exports = class extends Generator {
   }
 
   writing() {
+    console.log(this.props);
+
     // Copy files from the template folder to the destination
     // Also passes in the props object for templating replacements
     this.fs.copyTpl(
-      this.templatePath('**/**'),
+      this.templatePath('webapp/**/**'),
       this.destinationPath(),
       this.props,
       null,
-      {globOptions: {dot: true}}
+      { globOptions: { dot: true } }
     );
+
+    // Copy configuration file based on AAD Endpoint Version (v1/v2)
+    this.fs.copy(
+      this.templatePath(`${this.props.aadVersion}/auth.js`),
+      this.destinationPath('./utilities/auth.js')
+    );
+
   }
 
   install() {
